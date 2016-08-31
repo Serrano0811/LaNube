f1ca67169d713208171c6c8ec2a5e068fd6836fa
Serrano0811 <antonio.serrano0811@gmail.com>
2016-08-31 13:01:19 -0700

    Actualizacion de diseño
    Se actualiza el diseño de nuevo

--- application/views/templates/head.php
+++ application/views/templates/head.php
@@ -8,3 +8,19 @@
 			<link rel="stylesheet" type="text/css" href="<?= base_url() ?>bootstrap/css/estilo_personal.css">
 		</head>
 		<body>
+
+		<!--script type="text/javascript">
+		$(document).ready(function(){
+		    $('li.dropdown a.dropdown-toggle').hover(function() {
+		      $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(200);
+		    }, function() {
+		      $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(200);
+		    });  
+		});
+		</script-->
+
+		<script type="text/javascript">
+			$('prueba').one("mouseenter", function() {
+				$('JQdropdown').fadeIn(1000);
+			});
+		</script>
